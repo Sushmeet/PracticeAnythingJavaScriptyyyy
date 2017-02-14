8eb780de96847266436c4a5687d3ef20626f9aa1
sushmeet sunger <sushmeet.sunger@gmail.com>
2017-02-13 23:12:38 -0500

    used the class Checkng to perform bank operations (#4)

--- objectOrientedProgramming/objectOrientedProgramming.js
+++ objectOrientedProgramming/objectOrientedProgramming.js
@@ -9,19 +9,34 @@ function Checking(amount) {
     this.balance = amount;
 
     this.deposit = function (amount) {
-        this.balance += this.balance + amount
+        this.balance = this.balance + amount
     };
 
     this.withdraw = function (amount) {
         if (amount <= this.balance) {
             this.balance = this.balance - amount;
         }
-        else  {
+        else if (amount > this.balance) {
 
+            console.log("insufficient funds")
         }
     };
 
     this.toString = function () {
-        console.log ("Balance is" + this.balance );
+        console.log("Balance is " + this.balance + " dollars");
     };
 }
+
+
+var checkingacct = new Checking(10);
+checkingacct.toString();
+
+checkingacct.deposit(5);
+checkingacct.toString();
+
+checkingacct.withdraw(200);
+checkingacct.toString();
+
+checkingacct.withdraw(5);
+checkingacct.toString();
+
